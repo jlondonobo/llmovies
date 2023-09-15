3a4d359897d774e2e0f6d1ae03f8bb6942564256
jhortizo <joseortiz1807@gmail.com>
2023-09-15 11:27:21 -0500

    ✨ Add other popular providers' ids

--- utils/enums.py
+++ utils/enums.py
@@ -2,8 +2,10 @@ from enum import Enum
 
 
 class Providers(str, Enum):
-    # AppleTV = "2"
+    AppleTV = "2"
     Netflix = "8"
     AmazonPrimeVideo = "9"
     AmazonVideo = "10"
     DisenyPlus = "337"
+    HBOMax = "384"
+    Hulu = "15"
