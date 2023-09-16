32ee0f7a23d33f229285a9a008711e977ffdfb88
jlondonobo <j.londonobo@gmail.com>
2023-09-15 19:52:55 -0500

    🐛 Debug messaging

--- utils/simple_chat.py
+++ utils/simple_chat.py
@@ -12,7 +12,8 @@ class TopicInput(BaseModel):
     topic: str
     genre: str | None
     media: Literal["TV", "Movie", ""] | None
-    
+
+
 class LLMoviesOutputError(Exception):
     pass
 
@@ -38,14 +39,10 @@ Your response will help filter some results, so don't say anything!
 If the user asks you anything different than movies or TV shows, respectfully stop the conversation.
 """
 
-
-def generate_response(input: str) -> TopicInput:
+def generate_response(input: str, messages: list[dict[str, str]]) -> TopicInput:
     response = openai.ChatCompletion.create(
         model=chat_model,
-        messages=[
-            {"role": "system", "content": PROMPT},
-            {"role": "user", "content": input},
-        ],
+        messages=messages + [{"role": "user", "content": input}],
     )
     return response["choices"][0]["message"]["content"]
 
@@ -62,36 +59,44 @@ def parse_response(response: str) -> TopicInput:
         print(e.errors())
         raise LLMoviesOutputError
 
+
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "What would you like to watch?"}
+        {"role": "system", "content": PROMPT},
+        {"role": "assistant", "content": "What would you like to watch?"},
     ]
 
+
+# Print messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-        
-        
-if user_input := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    is_json_message = False
+    try:
+        json.loads(message["content"])
+        is_json_message = True
+    except ValueError:
+        pass
+    if (message["role"] != "system") and (not is_json_message):
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+
+if user_input := st.chat_input(
+    "Your question"
+):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
         st.write(user_input)
-    
-    
+
+
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_response(user_input) 
+            response = generate_response(user_input, st.session_state.messages)
             try:
                 parsed = parse_response(response)
+                st.image("https://t3.ftcdn.net/jpg/01/44/89/98/360_F_144899855_tqFVpAifgNGq7Uti1XoANq0mvsg0518o.jpg")
             except LLMoviesOutputError:
                 st.write(response)
-            
+
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
-    
-try:
-    st.write(parsed)
-except NameError:
-    pass
-    
