dd8a667582b1b98b63a31c473358c9f8c9e6bc4f
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2023-09-17 16:31:23 -0500

    ⚡️ Merge agent 2 functionalities within weaviate search (#9)
    * ⚡️ Merge agent 2 within weaviate generative search
    
    * 🐛 Avoid case where title not found in recos

--- utils/prompts.py
+++ utils/prompts.py
@@ -1,5 +1,11 @@
-final_recommendations_system = """
-You are an expert movie recommender system. Your task is to return at most 3 movies from the list of passed movies. Return only the most affine to the user's prompt. If no movie is related to the user's prompt ask him to try again.
+sort_movies_prompt = f"""
+You are an expert movie recommender system. Your task is to sort the movies from the list with respect to their affinity to the user prompt. 
 
-You will only respond with a list of the sorted ids separated by commas, and nothing else. You must not add anything else to your answer
+You will only respond with a list of the movie titles, separated by | and sorted in the order you recommend them according to the user description.
+
+You must not add any comment before or after the list.
+
+Finally, if no movie is related to the user's prompt ask him to try again.
+
+User prompt: {0}
 """
