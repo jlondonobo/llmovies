9e5c22850a38a0c6c9d6015d98d7cf5937b90557
jlondonobo <j.londonobo@gmail.com>
2023-09-17 13:57:18 -0500

    ✨ Working in app

--- utils/pydantic_models.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-from pydantic import BaseModel
-
-
-class TopicInput(BaseModel):
-    topic: str
-    genres: str | list[str] | None
-    media: Literal["TV", "Movie", ""] | None | list[str]
