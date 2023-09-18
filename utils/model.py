071e6607bf606a6e0995c5298888b1c49178a16f
jlondonobo <j.londonobo@gmail.com>
2023-09-17 22:49:18 -0500

    ✨ Update embeddings model

--- utils/model.py
+++ utils/model.py
@@ -1,4 +1,4 @@
 from sentence_transformers import SentenceTransformer
 
 # Use SentenceTransformers to get embeddings
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("BAAI/bge-small-en-v1.5")
